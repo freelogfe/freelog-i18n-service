7e845cda0cbb1c898f7abbfbeb0255ec56605be7
wwzh <790727372@qq.com>
2020-02-25 11:15:05 +0800

    「部署测试」：新增API

--- app/controller/home.js
+++ app/controller/home.js
@@ -7,6 +7,13 @@ class HomeController extends Controller {
   async index(ctx) {
     ctx.success('Hello, I\'m an i18n-service!')
   }
+
+  async helloPackageJson() {
+    const path = require('path')
+    const fse = require('fs-extra')
+    const result = fse.readJSONSync(path.resolve(process.cwd(), 'package.json'))
+    this.ctx.success(result)
+  }
 }
 
 module.exports = HomeController
--- app/router-map.js
+++ app/router-map.js
@@ -3,4 +3,5 @@
 // method path controller
 module.exports = {
   'get /v1/i18n/index': 'home.index',
+  'get /v1/i18n/readPackageJson': 'home.helloPackageJson',
 }
